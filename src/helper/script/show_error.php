4829101e1e28a0cc76fd7f113d8878cc59e37f69
André Ricardo <contatoadmx@gmail.com>
2023-11-09 16:31:31 -0300

    no report notice warning

--- src/helper/script/show_error.php
+++ src/helper/script/show_error.php
@@ -1,7 +1,7 @@
 <?php
 
-if (env('SHOW_ERROR')) {
+if (env('DEV')) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 }
